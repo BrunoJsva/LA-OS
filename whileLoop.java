09ed2ebcd1ce062a37c435bd6a558763214f0be5
Bruno Silva <bruno.jesuspessoal@gmail.com>
2022-08-08 16:43:19 -0300

    DO-WHILE

--- whileLoop.java
+++ whileLoop.java
@@ -1,12 +1,12 @@
 public class whileLoop {
     public static void main(String[] args){
-        int i = 1;
+        int i = 20;
         while(i < 10){
             System.out.println(i);
             i++;
         }
-
-        int j = 1;
+        //No método do-while executa pelo menos uma vez o código porque a verificação da condição e feita depois.
+        int j = 20;
         do {
             System.out.println(j);
             j++;
